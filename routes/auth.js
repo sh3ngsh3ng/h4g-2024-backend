72dd94c4fa3bab7bc7b1969983fbb55fd243280b
zhanyang01 <ngzhanyang01@gmail.com>
2024-02-09 18:53:11 +0800

    added retrieve user and update user

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { register, login } from "../controllers/auth";
+import { register, login, getUserById, updateUser } from "../controllers/auth";
 import { authMiddleware, registerMiddleware } from "../middleware/auth-middleware";
 
 const router = express.Router();
 
 router.post("/register", registerMiddleware, register);
 router.post("/login", authMiddleware, login);
+router.get("/user/:uid", getUserById);
+router.put("/userUpdate/:uid", updateUser);
 
 module.exports = router;
